b0682dfc7a9a4a8832957ea26a4a2e4521fb88e8
Omar De Donato <omar.dedonato@gmail.com>
2013-03-17 19:24:30 +0000

    first release

---
+++ src/it/visit/ravenna/MainActivity.java
@@ -0,0 +1,15 @@
+package it.visit.ravenna;
+
+import android.os.Bundle;
+import org.apache.cordova.*;
+
+public class MainActivity extends DroidGap {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
+        super.loadUrl("file:///android_asset/www/index.html");
+    }
+    
+}
